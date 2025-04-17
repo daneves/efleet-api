46250eb6ded2b132adb8dfaaaed15355157a2195
daneves <danielj.neves@hotmail.com>
2025-04-17 10:08:26 -0300

    Update rezultz-lead.ts

--- rezultz-lead.ts
+++ rezultz-lead.ts
@@ -1,55 +1,60 @@
 import axios from 'axios';
 
 export async function createLead(leadData: any) {
-  const { name, email, phone, company, cnpj } = leadData;
+  try {
+    const { name, email, phone, company, cnpj } = leadData;
 
-  const auth = await axios.post(
-    'https://api-auth.rezultz.com.br/oauth2/token',
-    new URLSearchParams({
-      grant_type: 'client_credentials',
-      client_id: '45oeothip7dekqhj7ppia7bbcf',
-      client_secret: '14bfnibalt9a9lrike4echvmsbt7u1gam4p7qgujbuh3va13mutf',
-    }),
-    {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    }
-  );
+    const auth = await axios.post(
+      'https://api-auth.rezultz.com.br/oauth2/token',
+      new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: '45oeothip7dekqhj7ppia7bbcf',
+        client_secret: '14bfnibalt9a9lrike4echvmsbt7u1gam4p7qgujbuh3va13mutf',
+      }),
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }
+    );
 
-  const token = auth.data.access_token;
+    const token = auth.data.access_token;
 
-  const response = await axios.post(
-    'https://api.rezultz.com.br/lead',
-    {
-      type: 'buyer',
-      referer: 'Formulário Site',
-      fingerprintId: '123',
-      policyAccept: true,
-      contact: {
-        name,
-        email,
-        mobile: phone,
+    const response = await axios.post(
+      'https://api.rezultz.com.br/lead',
+      {
+        type: 'buyer',
+        referer: 'Formulário Site',
+        fingerprintId: '123',
+        policyAccept: true,
+        contact: {
+          name,
+          email,
+          mobile: phone,
+        },
+        company: {
+          name: company,
+          document: cnpj,
+        },
+        creditInfo: {
+          availableLimit: 0,
+          requestLimit: 0,
+          availableTerm: 0,
+          requestTerm: 0,
+        },
+        seller: {
+          name: company,
+        },
       },
-      company: {
-        name: company,
-        document: cnpj,
-      },
-      creditInfo: {
-        availableLimit: 0,
-        requestLimit: 0,
-        availableTerm: 0,
-        requestTerm: 0,
-      },
-      seller: {
-        name: company,
-      },
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-  return response.data;
+    return response.data;
+  } catch (error: any) {
+    console.error('Erro dentro do createLead:', error.response?.data || error.message || error);
+    throw error;
+  }
 }
