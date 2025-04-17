51b58c9c602f0d534e5e3d00f1889dca754e7fa7
daneves <danielj.neves@hotmail.com>
2025-04-17 10:38:57 -0300

    Update leadRouter.ts

--- leadRouter.ts
+++ leadRouter.ts
@@ -8,7 +8,6 @@ leadRouter.post('/', async (req: Request, res: Response) => {
     const response = await createLead(req.body);
     res.status(200).json({ success: true, response });
   } catch (error) {
-    console.error('Erro ao criar lead:', error);
     res.status(500).json({ error: 'Erro ao criar lead' });
   }
 });
