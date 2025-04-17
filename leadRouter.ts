07e9009ada6bb8984d60ddd0698be78fa8ea32c0
daneves <danielj.neves@hotmail.com>
2025-04-17 09:49:12 -0300

    Update leadRouter.ts

--- leadRouter.ts
+++ leadRouter.ts
@@ -3,7 +3,7 @@ import { createLead } from './rezultz-lead';
 
 export const leadRouter = Router();
 
-leadRouter.post('/lead', async (req: Request, res: Response) => {
+leadRouter.post('/', async (req: Request, res: Response) => {
   try {
     const response = await createLead(req.body);
     res.status(200).json({ success: true, response });
