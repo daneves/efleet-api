4849fc7ebf29cabf63a830955a65b69f16149e4b
daneves <danielj.neves@hotmail.com>
2025-04-17 12:11:26 -0300

    Update leadRouter.ts

--- leadRouter.ts
+++ leadRouter.ts
@@ -4,14 +4,19 @@ import { createLead } from './rezultz-lead';
 export const leadRouter = Router();
 
 leadRouter.post('/', async (req: Request, res: Response) => {
-  console.log('🔥 Dados recebidos no body:', req.body); // <- AQUI
+  console.log('🔥 Dados recebidos no body:', req.body);
 
   try {
     const response = await createLead(req.body);
     res.status(200).json({ success: true, response });
-  } catch (error) {
-    console.error('Erro dentro do createLead:', error.response?.data || error.message || error);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Erro ao criar lead:', error.message);
+    } else {
+      console.error('Erro ao criar lead:', error);
+    }
     res.status(500).json({ error: 'Erro ao criar lead' });
   }
 });
+
 
